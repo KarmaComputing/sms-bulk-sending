650178f5f4eca851a29054be6b1496a558122231
Joel <joel@karmacomputing.co.uk>
2023-04-24 20:46:10 -0400

    adding secure_filename and error handling

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from flask import (
 )
 import os
 from dotenv import load_dotenv
-from utils import send_sms, extract_customer_numbers_from_spreadsheet
+from utils import send_sms, extract_customer_numbers_from_spreadsheet, allowed_file
+from werkzeug.utils import secure_filename
 
 load_dotenv()
 
@@ -20,21 +21,27 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 @app.route("/", methods=["GET", "POST"])
 def upload():
     if request.method == "POST":
-        if "file" not in request.files:
-            flash("No file selected")
-            return render_template("index.html")
         file = request.files["file"]
-        if file.filename == "":
+        if "file" not in request.files or file.filename == "":
             flash("No file selected")
             return render_template("index.html")
-        else:
+        elif allowed_file(file.filename):
             # Save the uploaded file to the uploads folder
-            filename = file.filename
+            filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             numbers_to_contact = extract_customer_numbers_from_spreadsheet(filename)
             for number in numbers_to_contact:
-                send_sms(number, "Mow your lawn")
+                message_sent = send_sms(number, "Mow your lawn")
+                if message_sent[0] == 500:
+                    flash(message_sent[1])
+                    flash(
+                        "Tip: Receiver number of the SMS message. No spaces, do not use the + before the international countrycode. Omit the first 0 from the number. Example: 31612345678"
+                    )
+                    return render_template("index.html")
             return redirect(url_for("download", filename=filename))
+        else:
+            flash("The file extension is not allowed")
+            return render_template("index.html")
 
     return render_template("index.html")
 
