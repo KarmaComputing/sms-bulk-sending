a677d378298aafaff3c812cd90e3cd370330917d
chrisjsimpson <chris.j.simpson@live.co.uk>
2023-05-03 21:37:49 +0100

    Fix #5 add confirmation step before sending SMS

--- src/web/app.py
+++ src/web/app.py
@@ -8,7 +8,8 @@ from flask import (
     redirect,
     url_for,
     flash,
-)
+    session,
+)  # noqa: E501
 import os
 from dotenv import load_dotenv
 from utils import (
@@ -26,6 +27,30 @@ app = Flask(__name__)
 UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", None)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.teardown_appcontext(close_db)
+
+
+@app.route("/confirm")
+def confirm():
+    """Confirm to the user that they are happy for the SMS messages to
+    be sent"""
+    try:
+        if "confirm" in request.args:
+            for number in session["numbers_to_contact"]:
+                message_sent = send_sms(number, "Mow your lawn")
+                if message_sent[0] == 500:
+                    flash(message_sent[1])
+                    flash("Tip: No spaces permitted in number")
+                    flash(
+                        "Do not use the + before the international countrycode."  # noqa: E501
+                    )  # noqa: E501
+                    flash(
+                        "Omit the first 0 from the number. Example: 31612345678"  # noqa: E501
+                    )  # noqa: E501
+
+    except Exception() as e:
+        log.error(f"Error sending SMS messages: {e}")
+        return "Error sending SMS messages", 500
+    return render_template("confirmation.html")
 
 
 @app.route("/health")
@@ -68,20 +93,12 @@ def index():
             numbers_to_contact = extract_customer_numbers_from_spreadsheet(
                 filename
             )  # noqa: E501
-            for number in numbers_to_contact:
-                message_sent = send_sms(number, "Mow your lawn")
-                if message_sent[0] == 500:
-                    flash(message_sent[1])
-                    flash("Tip: No spaces permitted in number")
-                    flash(
-                        "Do not use the + before the international countrycode."  # noqa: E501
-                    )  # noqa: E501
-                    flash(
-                        "Omit the first 0 from the number. Example: 31612345678"  # noqa: E501
-                    )  # noqa: E501
 
-                    return render_template("index.html")
-            return redirect(url_for("download", filename=filename))
+            # Store numbers to contact in session
+            session["numbers_to_contact"] = numbers_to_contact
+
+            # Redirect to confirmation before sending sms messages
+            return redirect(url_for("confirm"))
         else:
             flash("The file extension is not allowed")
             return render_template("index.html")
